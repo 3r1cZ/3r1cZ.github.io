dc91c8872ada043689be3f18ed84e008d567e0e1
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2024-01-02 01:31:05 -0500

    Remove navbar collapsing and toggler icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,10 @@ const Navbar = () => {
   const [hovered, setHovered] = useState(false);
 
   return (
-    <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <nav
+      className="navbar navbar-expand sticky-top bg-primary "
+      data-bs-theme="dark"
+    >
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <img
@@ -20,19 +23,8 @@ const Navbar = () => {
             onMouseLeave={() => setHovered(false)}
           />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">
                 Home
